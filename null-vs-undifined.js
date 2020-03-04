0143cb78dbafb0dda2bcd9675476687be0470ada
Abhi Mohajan <abhimohajan@gmail.com>
2020-03-04 18:57:37 +0600

    null vs undefined cleared for interview question

---
+++ null-vs-undifined.js
@@ -0,0 +1,12 @@
+// let fatherName;
+// console.log(fatherName);
+
+// function add(num, num2){
+//     console.log(num + num2);
+//     return num2;
+// }
+// const result = add(10);
+// console.log(result);
+
+const student = {name: "abhi", phone:74554541};
+console.log(student.gf);
