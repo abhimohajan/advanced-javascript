b12e8c63f91e8d01b38bd9e6ee1cadd796dde1fc
Abhi Mohajan <abhimohajan@gmail.com>
2020-03-04 18:27:32 +0600

    True vs Flase

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// falsy condition
+// 0
+// false
+// null
+// NaN
+// undefined
+// truthy Condition
+// "0", '0', " " ' '
+
+
+
+
+let name = "";
+if(name){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
